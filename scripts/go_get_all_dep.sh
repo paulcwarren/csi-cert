7c1f2549904f2ce2379c1aed35de144e8882bb49
Paul Warren <paul.warren@emc.com>
2017-09-14 15:05:27 -0700

    go get github.com/container-storage-interface/spec -> github.com/container-storage-interface/spec/lib/go
    [151024547](https://www.pivotaltracker.com/story/show/151024547)

--- scripts/go_get_all_dep.sh
+++ scripts/go_get_all_dep.sh
@@ -13,6 +13,6 @@ go get -u "golang.org/x/net/context"
 echo "installing grpc..."
 go get -u "google.golang.org/grpc"
 echo "installing csi spec..."
-go get -u "github.com/container-storage-interface/spec"
+go get -u "github.com/container-storage-interface/spec/lib/go"
 
 echo "done."
