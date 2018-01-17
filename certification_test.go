adafd2ed1016ec4e48c9dc1e331189fa0ef297b3
Paul Warren <paul.warren@emc.com>
2018-01-17 15:55:37 -0800

    whoops
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- certification_test.go
+++ certification_test.go
@@ -15,8 +15,6 @@ import (
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/paulcwarren/csi-cert"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var isSeeded = false
